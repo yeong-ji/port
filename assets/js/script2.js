5faf3dce29239e564c8e3116d01791ce296df940
yeong-ji <72494527+yeong-ji@users.noreply.github.com>
2021-01-27 18:50:33 +0900

    port

---
+++ assets/js/script2.js
@@ -0,0 +1,313 @@
+(function(){
+    //로딩이후 실행
+    function loading(){
+        const progress = $(".progress"),
+            progressText = progress.find(".progress-text");
+
+        let imgLoad = imagesLoaded("body"),
+            imgTotal = imgLoad.images.length,
+            imgLoaded = 0,
+            imgCurrent = 0,
+            progressTimer = setInterval(updateProgress, 500/60);
+
+        imgLoad.on("progress",function(){
+            imgLoaded++;
+        })
+
+        function updateProgress(){
+            let target = (imgLoaded / imgTotal) * 100;
+
+            imgCurrent += (target - imgCurrent) * 0.1;
+            progressText.text(Math.floor(imgCurrent) + "%");
+
+            if(imgCurrent >= 100) {
+                clearInterval(progressTimer)
+                progress.delay(1000).fadeOut(1000)
+                setTimeout(function(){
+                    //2초후 등장 애니메이션
+                    let tl = gsap.timeline();
+                    tl.fromTo(".main-img", {scale: 0}, {opacity: 1, scale: 1, duration:1.5, ease: "elastic.out(1.35, 0.75)"})
+                    .fromTo(".main-cont h1", {y:-80}, {opacity:1, y:0, duration:1, ease: "back.out(3)"},"-=0.5")
+                    .fromTo(".main-t1",{y:10}, {opacity:1, y:0, stagger:0.1})
+                    .fromTo(".main-t2",{y:10}, {opacity:1, y:0, stagger:0.1},"-=2.1")
+                    .set("#main .main-cont p", {className:"+=show"})
+                    .fromTo("#header .logo a", {scale: 0}, {opacity:1, scale: 1, duration:1, ease: "back.out(2)"})
+                    .fromTo("#header .sign li", {scale: 0}, {opacity:1, scale: 1, stagger:0.3, ease: "back.out(2)"})
+                    .to("#header .menu-line", {x:0, duration:1},"-=1.9");
+            
+                    // 마우스 방향에 따른 이미지
+                    let x = 0,
+                        y = 0,
+                        mouseX = 0,
+                        mouseY = 0,
+                        angleX = 0,
+                        angleY = 0,
+                        fmouseX = 0,
+                        fmouseY = 0;
+                    
+                    const mainImg = document.querySelector('.main-img');
+            
+                    function handleMove(event) {
+                        x = event.pageX;
+                        y = event.pageY;
+                        //console.log(x, y)
+                        //mouseX = window.innerWidth / 2 - x; //마우스 x좌표값 가운데
+            
+                    // 마우스 위치 최대 50 , 최소 -50
+                    mouseX = Math.max(-50, Math.min(50, window.innerWidth / 2 - x ));
+                    mouseY = Math.max(-50, Math.min(50, window.innerHeight / 2 - y));
+            
+                    angleX = (12 * mouseX) / 100; // 계산
+                    angleY = (12 * mouseY) / 100;
+            
+                    fmouseX += (angleX - fmouseX) * 0.1 // = 1 / 10
+                    fmouseY += (angleY - fmouseY) * 0.1 // = 1 / 10
+                    
+                    mainImg.style.transform = `translate(-50%, -50%) perspective(600px) rotateX(${fmouseY}deg) rotateY(${-fmouseX}deg)`// 요소 3D 효과
+                    document.querySelector('.main-text-wrap').style.transform = `perspective(1200px) rotateX(${fmouseY}deg) rotateY(${-fmouseX}deg)`
+                    }
+            
+                    window.addEventListener("mousemove", handleMove);
+                },2200);
+            }
+            if(imgCurrent > 99) {
+                imgCurrent = 100;
+            } 
+        }
+    }
+    loading ();
+
+    //메뉴클릭 이벤트
+    document.querySelector('#header .menu .hamburger').addEventListener('click', function(){
+        document.querySelector('#header .menu').classList.add('close');
+        document.querySelector('#header .menu .nav').classList.add('active');
+        document.querySelector('#header .menu .nav').classList.remove('not_active');
+    });
+
+    //메뉴 애니메이션
+    let navLi = document.querySelectorAll('#header .menu .nav ul li')
+    navLi[0].firstChild.addEventListener('click',function(){
+        setTimeout(function(){
+            document.querySelector('#header .menu').classList.remove('close');
+        },2200)
+            document.querySelector('#header .menu .nav').classList.remove('active');
+            document.querySelector('#header .menu .nav').classList.add('not_active');
+    })
+    navLi[1].firstChild.addEventListener('click',function(e){
+        setTimeout(function(){
+            document.querySelector('#header .menu').classList.remove('close');
+        },2200)
+            document.querySelector('#header .menu .nav').classList.remove('active');
+            document.querySelector('#header .menu .nav').classList.add('not_active');
+    })
+    navLi[2].firstChild.addEventListener('click',function(e){
+        document.querySelector('#port2').classList.remove('yellow');
+        setTimeout(function(){
+            document.querySelector('#header .menu').classList.remove('close');
+        },2200)
+            document.querySelector('#header .menu .nav').classList.remove('active');
+            document.querySelector('#header .menu .nav').classList.add('not_active');
+    })
+    navLi[3].firstChild.addEventListener('click',function(e){
+        setTimeout(function(){
+            document.querySelector('#header .menu').classList.remove('close');
+        },2200)
+            document.querySelector('#header .menu .nav').classList.remove('active');
+            document.querySelector('#header .menu .nav').classList.add('not_active');
+    })
+
+    document.querySelector('#header .menu .hamburger-close').addEventListener('click', function(){
+        setTimeout(function(){
+            document.querySelector('#header .menu').classList.remove('close');
+        },2200)
+        document.querySelector('#header .menu .nav').classList.remove('active');
+        document.querySelector('#header .menu .nav').classList.add('not_active');
+    });
+
+    // 흐르는 글씨
+
+    let speed = 0
+    let speed2 = 0
+    let speed3 = 0
+    let speed4 = 0
+
+    let acc = 0
+    let hover = false
+    let width
+    let width2
+    let width3
+    let width4
+
+    let times
+    let times2
+    let times3
+    let times4
+
+    let cloned = []
+    let cloned2 = []
+    let cloned3 = []
+    let cloned4 = []
+
+    const item = document.querySelector('.about-text-wrap div:nth-child(2)')
+    const word = item.querySelector('span')
+    const item2 = document.querySelector('.about-text-wrap div:nth-child(1)')
+    const word2 = item2.querySelector('span')
+    const item3 = document.querySelector('.contact-text-wrap div:nth-child(2)')
+    const word3 = item3.querySelector('span')
+    const item4 = document.querySelector('.contact-text-wrap div:nth-child(1)')
+    const word4 = item4.querySelector('span')
+    
+
+    /* 계산 */
+    const calculate = () => {
+    // console.log('cloned', cloned)
+    cloned.forEach(i => {
+        i.parentNode.removeChild(i)
+    })
+    cloned2.forEach(i => {
+        i.parentNode.removeChild(i)
+    })
+    cloned3.forEach(i => {
+        i.parentNode.removeChild(i)
+    })
+    cloned4.forEach(i => {
+        i.parentNode.removeChild(i)
+    })
+    
+    //forEach 배열에 담겨있는 데이터를 각각 한번씩 함수실행  (화면크기에 따라 늘어나는 span 삭제)
+    cloned = []
+    cloned2 = []
+    cloned3 = []
+    cloned4 = []
+    
+    width = Math.ceil(word.clientWidth)  //ceil 올림 , clientWidth테두리를 뺀 엘리먼트의 안쪽 폭
+    times = Math.ceil(window.innerWidth / width) // span 추가할 갯수
+
+    width2 = Math.ceil(word2.clientWidth)
+    times2 = Math.ceil(window.innerWidth / width2)
+
+    width3 = Math.ceil(word3.clientWidth)
+    times3 = Math.ceil(window.innerWidth / width3)
+
+    width4 = Math.ceil(word4.clientWidth)
+    times4 = Math.ceil(window.innerWidth / width4)
+    
+   // item.style.width = `${(times + 1) * width}px`
+
+    for (let i = 0; i < times + 1; i++) {
+        const clone = word.cloneNode(true)
+        word.parentNode.appendChild(clone)
+        cloned.unshift(clone)
+    }
+
+    for (let i = 0; i < times2 + 1; i++) {
+        const clone2 = word2.cloneNode(true)
+        word2.parentNode.appendChild(clone2)
+        cloned2.push(clone2)
+    }
+
+    for (let i = 0; i < times3 + 1; i++) {
+        const clone3 = word3.cloneNode(true)
+        word3.parentNode.appendChild(clone3)
+        cloned3.push(clone3)
+    }
+
+    for (let i = 0; i < times4 + 1; i++) {
+        const clone4 = word4.cloneNode(true)
+        word4.parentNode.appendChild(clone4)
+        cloned4.push(clone4)
+    }
+
+    }
+    
+
+    /* Listeners 마우스 오버 효과 */
+    const handleMouse = bool => hover = bool
+    function itemEvent(item){
+        item.addEventListener('mouseenter', () => {handleMouse(true)})
+        //item.addEventListener('touchstart', () => {handleMouse(true)})
+        item.addEventListener('mouseleave', () => {handleMouse(false)})
+        item.addEventListener('touchend', () => {handleMouse(false)})
+    }
+    window.addEventListener('resize', calculate)
+    window.addEventListener('load', calculate)
+    itemEvent(item);
+    itemEvent(item2);
+    itemEvent(item3);
+    itemEvent(item4);
+   
+
+    /* 애니메이션 */
+    const animate = () => {
+    // 가속도
+    acc += 0.1
+    if (hover) {
+        acc -= 0.35
+    }
+    
+    // 최소/최대 가속도
+    acc = Math.min(2, Math.max(0, acc)) 
+    
+    // 속도변수에 가속도 추가
+    speed += acc
+    speed2 += acc
+    speed3 += acc
+    speed4 += acc
+    
+    // Text Loop
+    if (speed >= width) {
+        speed = 0
+    }
+    if (speed2 >= width2) {
+        speed2 = 0
+    }
+    if (speed3 >= width3) {
+        speed3 = 0
+    }
+    if (speed4 >= width4) {
+        speed4 = 0
+    }
+    // CSS Text
+    item.style.transform = `translateX(${speed - 7252}px)`    //6620
+    item2.style.transform = `translateX(${-speed2}px)`
+
+    
+    item3.style.transform = `translateX(${speed3 - 4788}px)`    //6620
+    item4.style.transform = `translateX(${-speed4}px)`
+    
+    
+    // 애니메이션이 부드럽게 나오도록 작업  [ 변화된 화면을 그릴 준비가 되었을 때 실행 ]
+    requestAnimationFrame(animate)
+    }
+    animate();
+
+
+    //이미지 변경
+
+    const aboutImg1 = document.querySelector('.about-cont2 .advan-img1 img');
+    const aboutImg2 = document.querySelector('.about-cont3 .advan-img1 img')
+   
+   
+    function advanImg(aboutImg, num) {
+        let image = 1;
+    setInterval(function(){
+        image++;
+        if(image>4) {image=1;}
+        aboutImg.src = `assets/img/about-img${num}-${image}.png`
+    },500)
+
+    }
+
+    advanImg(aboutImg1, 1);
+    advanImg(aboutImg2, 2);
+
+    const profileImg = document.querySelector('.profile-img img');
+    
+    let profileImage = 1;
+    setInterval(function(){
+        profileImage++;
+        if(profileImage>4) {profileImage=1;}
+        profileImg.src = `assets/img/profile-img${profileImage}.png`
+    },500);
+
+}())
---
+++ assets/js/scrollscript2.js
@@ -0,0 +1,245 @@
+(function(){
+     
+
+    //Latest &nbsp; work 글자 쪼갬
+       const spanElement = document.querySelector('#port2 .publi-title span');
+       const spans = spanElement.innerText.split('').map((elem) => `<span>${elem}</span>`).join('');
+
+       spanElement.innerHTML = spans;
+   //스킬 흐르는 글씨 생성
+       for(let i = 1; i <= 6; i++) {
+           const skillBanner = document.querySelector('#skill-banner .skill-banner-cont .skill-banner-text-wrap');
+           const skillDiv = document.createElement('div');
+         
+           skillDiv.innerHTML = `<span aria-hidden="true">HTML</span><span aria-hidden="true">CSS</span><span aria-hidden="true">JAVASCRIPT</span><span aria-hidden="true">JQUERY</span><span aria-hidden="true">PHOTOSHOP</span><span aria-hidden="true">ILLUSTRATOR</span>`;
+   
+           skillBanner.append(skillDiv);
+       }
+
+   //애니메이션, 스크립트 흐르는 글씨 생성
+       for(let i = 0; i < 4; i++) {
+           const workBanner = document.querySelector('#work-banner .work-banner-cont .work-banner-text-wrap');
+           const workDiv = document.createElement('div');
+         
+           workDiv.innerHTML = `<span aria-hidden="true">ANIMATION</span><span aria-hidden="true">JAVASCRIPT</span><span aria-hidden="true">ANIMATION</span><span aria-hidden="true">JAVASCRIPT</span>`;
+   
+           workBanner.append(workDiv);
+       }
+   // 글자 쪼갬
+       const port4Ani = document.querySelector('#port4 .ani-title h2 span');
+       const port4AniSpan = port4Ani.innerText.split('').map((elem) => `<span>${elem}</span>`).join('');
+
+       port4Ani.innerHTML = port4AniSpan;
+
+       const port4Scr = document.querySelector('#port4 .script-title h2 span');
+       const port4ScrSpan = port4Scr.innerText.split('').map((elem) => `<span>${elem}</span>`).join('');
+
+       port4Scr.innerHTML = port4ScrSpan;
+   
+   //스크롤 등장 gsap
+   window.addEventListener("scroll", function(){
+       let scroll = window.pageYOffset;
+
+       document.querySelector('.scroll').textContent = parseInt(scroll);
+
+       //$('.scroll').text(scroll);
+
+       // 장단점 등장씬
+       if(scroll > $(".about-cont2").offset().top - $(window).height()*0.7){
+           gsap.to(".about-cont2 .advan-img1", {duration: 2, scale: 1, rotation:0, opacity:1 ,ease: "expo.out"})
+           let about1 = gsap.timeline();
+           about1.to(".about-cont2 .advan-text-wrap .advan-t1", {opacity:1, y:0, stagger:0.1, ease: "back.inOut(3)"})
+           .set(".about-cont2 .advan-text-wrap h3", {className:"+=show"})
+           .to(".about-cont2 .advan-text-wrap>span", {opacity:1, x:0, duration: 0.5},"-=1")
+           .to(".about-cont2 .advan-text-wrap div", {opacity:1, x:0, duration:1})
+           .to(".about-cont2 .advan-text-wrap div.advan-btn", {y:0, duration:1, ease: "back.out(1.1)"},"-=1");
+       }
+
+       if(scroll > $(".about-cont3").offset().top - $(window).height()*0.7){
+           gsap.to(".about-cont3 .advan-img1", {duration: 2, scale: 1, rotation:0, opacity:1 ,ease: "expo.out"})
+           let about2 = gsap.timeline();
+           about2.to(".about-cont3 .advan-text-wrap .advan-t2", {opacity:1, y:0, stagger:0.1, ease: "back.inOut(3)"})
+           .set(".about-cont3 .advan-text-wrap h3", {className:"+=show"})
+           .to(".about-cont3 .advan-text-wrap>span", {opacity:1, x:0, duration: 0.5},"-=1")
+           .to(".about-cont3 .advan-text-wrap div", {opacity:1, x:0, duration:1})
+           .to(".about-cont3 .advan-text-wrap div.advan-btn", {y:0, duration:1, ease: "back.out(1.1)"},"-=1");
+       }
+
+       if(scroll > $(".about-cont4").offset().top - $(window).height()*0.7){
+           gsap.to(".about-cont4 .profile-img", {duration: 2, scale: 1, rotation:0, opacity:1 ,ease: "expo.out"})
+           let about3 = gsap.timeline();
+           about3.to(".about-cont4 .profile-text-wrap h3 span", {opacity:1, y:0, stagger:0.1, ease: "back.inOut(1)"})
+           .set(".about-cont4 .profile-text-wrap h3", {className:"+=show"})
+           .to(".about-cont4 .profile-text-wrap>span", {opacity:1, x:0, duration: 0.5},"-=1")
+           .to(".about-cont4 .profile-text>ul>li", {x:0, opacity:1, duration: 1})
+           .to(".about-cont4 .profile-text li em", {x:0, opacity:1, duration:1},"-=1")
+           .to("#about .about-cont4 .profile-btn", {scale:1, opacity:1, duration:1, ease: "back.out(1.7)"})
+       }
+
+      
+       //스킬 돌기
+       if(scroll > $("#skills").offset().top - $(window).height()/2){
+           gsap.to("#skills .skills-cont h2", {duration: 1, opacity: 1, scale:1, ease: "back.out(1.7)"})
+       }
+
+       let rotate = (scroll - $("#skills .skills-cont").offset().top + ($(window).height() / 2) ) * 0.4;
+       
+       $("#skills .skills-cont .skills-img img").css("transform","rotate("+rotate+"deg)");
+     
+       if( rotate > -60 && rotate < 50 ) {
+           $("#skills .skills-cont ul li").eq(0).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(2).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(4).find(".skill-box").removeClass("show");
+       }else if( rotate > 50 && rotate < 90 ) {
+           $("#skills .skills-cont ul li").eq(2).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(0).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(4).find(".skill-box").removeClass("show");
+       }else if(rotate > 90 && rotate < 132) {
+           $("#skills .skills-cont ul li").eq(4).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(2).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(5).find(".skill-box").removeClass("show");
+       }else if(rotate > 132 && rotate < 230) {
+           $("#skills .skills-cont ul li").eq(5).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(4).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(3).find(".skill-box").removeClass("show");
+       } else if(rotate > 230 && rotate < 272) {
+           $("#skills .skills-cont ul li").eq(3).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(5).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(1).find(".skill-box").removeClass("show");
+       } else if(rotate > 272 && rotate < 311) {
+           $("#skills .skills-cont ul li").eq(1).find(".skill-box").addClass("show");
+           $("#skills .skills-cont ul li").eq(3).find(".skill-box").removeClass("show");
+       } else {
+           $("#skills .skills-cont ul li").eq(1).find(".skill-box").removeClass("show");
+           $("#skills .skills-cont ul li").eq(0).find(".skill-box").removeClass("show");
+       }
+
+       //포트폴리오 글씨 커지게
+       if(scroll > $('#port1').offset().top && scroll < 7000 ){
+          $('#port1').addClass('fixed');
+       }
+       if(scroll > $('#port2').offset().top + $(window).height()/2 && scroll < $('#port2 .publi-box:last-child').offset().top + $(window).height()*2 ) {
+           $('#contents').addClass('black');
+       }else {
+           $('#contents').removeClass('black');
+       }
+       
+       
+       let scale = (scroll - $("#skills").offset().top - $(window).height()) * 0.04
+       if(scale >= 1 && scale <= 100 ) {
+           $("#port1 .port-title .port-title-wrap h2").css("transform","scale("+scale+") translateX(-4%)");
+       } else if(scale > 100) {
+           $("#port1 .port-title .port-title-wrap h2").css("transform","scale(100) translateX(-4%)");
+       } else {
+           $("#port1 .port-title .port-title-wrap h2").css("transform","scale(1)");
+       }
+
+       if(scroll > 7001){
+           $('#port1').removeClass('fixed');
+       }
+       if(scroll < $('#skills').offset().top + $(window).height()) {
+           $('#port1').removeClass('fixed');
+       }
+
+       if(scroll > $('#port2').offset().top + $(window).height()/2) {
+           $('#port2').removeClass('yellow');
+       }else{
+           $('#port2').addClass('yellow');
+       }
+
+       
+       //포트폴리오 
+
+       if(scroll > $('#port2 .publi-title').offset().top - $(window).height()*0.8){
+           let publi1 = gsap.timeline();
+           publi1.to("#port2 .publi-title>span span", {opacity:1, rotation:0, y:0, stagger:0.1, ease: "power4.out"})
+           .set("#port2 .publi-title>span", {className:"+=show"})
+       }
+
+       for(let i = 0; i < $('.publi-box').length; i++) {
+           if(scroll > $('.publi-box').eq(i).offset().top - $(window).height()*0.7) {
+               let portNum = i+2;
+               let port2Box1 = gsap.timeline();
+               port2Box1.to("#port2 .publi-box:nth-child("+portNum+") .publi-text-wrap h3 span", {opacity:1, y:0, stagger:0.1, ease: "back.inOut(1)"})
+                        .to("#port2 .publi-box:nth-child("+portNum+") .publi-text-wrap>span", {opacity:1, x:0, duration:1, ease: "expo.out"})
+                        .to("#port2 .publi-box:nth-child("+portNum+") .publi-text-wrap .publi-text", {opacity:1, x:0, duration:1, ease: "expo.out"},"-=1")
+                        .to("#port2 .publi-wrap .publi-box:nth-child("+portNum+") .publi-btn", {y:0, opacity:1, duration:0.5, ease: "back.out(1.1)"})
+          }
+       }
+
+
+      function viewed(number){
+           if(scroll > $('.publi-box').eq(number).offset().top - $(window).height()*1.3){
+               $('.publi-box').eq(number).children('.publi-img').addClass('viewed');
+           } 
+      }
+      viewed(0);
+      viewed(1);
+      viewed(2);
+      viewed(3);
+
+      
+
+      //애니메이션 글씨 작아지게
+
+       if(scroll > $('#port2 .publi-box:last-child').offset().top + $(window).height() &&  scroll < $("#port4").offset().top - 1){
+           $('#port3').addClass('fixed');
+       }
+   
+       if(scroll < $('#port2 .publi-box:last-child').offset().top + $(window).height() + 1 ){
+           $('#port3').removeClass('fixed');
+       }
+       
+       
+       let scale2 = -((scroll - $("#port4").offset().top + $(window).height()) * 0.05)
+       if(scale2 >= 1) {
+           $("#port3 .port3-title-wrap h2").css("transform","scale("+scale2+") translateX(-3%)");
+       } else if(scale2 < 0) {
+           $("#port3 .port3-title-wrap h2").css("transform","scale(1)");
+       }
+
+       if(scroll > $('#port4').offset().top - $(window).height()) {
+           $('#port3').removeClass('fixed');
+       }
+
+       let port4 = ['ani', 'script'];
+       for(let i = 0; i < port4.length; i++){
+           //console.log(port4[i])
+           if(scroll > $('#port4 .'+port4[i]+'-wrap').offset().top - $(window).height()*0.7){
+               let port4Ani = gsap.timeline();
+               port4Ani.to("#port4 ."+port4[i]+"-title h2 span span", {y:0, opacity:1, stagger:0.1, ease: "back.inOut(1)"})
+                       .set("#port4 ."+port4[i]+"-title h2>span", {className:"+=show"})
+                       .set("#port4 ."+port4[i]+"-title h2", {className:"+=show"});
+   
+               $("#port4 ."+port4[i]+"-cont:nth-child(2), #port4 ."+port4[i]+"-cont:nth-child(3)").addClass('show');
+           }
+   
+           if(scroll > $('#port4 .'+port4[i]+'-wrap').offset().top + $(window).height()/10){
+               $("#port4 ."+port4[i]+"-cont:nth-child(4), #port4 ."+port4[i]+"-cont:nth-child(5)").addClass('show');
+           }
+   
+           
+       }
+
+       let translate1 = -((scroll - $(".ani-wrap").offset().top) * 0.05)
+        //console.log("translate1: "+translate1)
+        if(translate1<=0) {
+            $("#port4 .ani-cont:nth-child(2n) .ani-img").css("transform", "translateY("+translate1*2+"px)");
+            $("#port4 .ani-cont:nth-child(2n+1) .ani-img").css("transform", "translateY("+translate1+"px)");
+            $("#port4 .ani-cont:nth-child(2n) .ani-text").css("transform", "translateY("+translate1*2+"px)");
+            $("#port4 .ani-cont:nth-child(2n+1) .ani-text").css("transform", "translateY("+translate1+"px)");
+        }
+
+        let translate2 = -((scroll - $(".script-wrap").offset().top) * 0.05)
+        //console.log("translate2: "+translate2)
+        if(translate2<=0) {
+        $("#port4 .script-cont:nth-child(2n) .script-img").css("transform", "translateY("+translate2+"px)");
+        $("#port4 .script-cont:nth-child(2n+1) .script-img").css("transform", "translateY("+translate2*2+"px)");
+        $("#port4 .script-cont:nth-child(2n) .script-text").css("transform", "translateY("+translate2+"px)");
+        $("#port4 .script-cont:nth-child(2n+1) .script-text").css("transform", "translateY("+translate2*2+"px)");
+        
+        }
+
+      
+   });
+}())
